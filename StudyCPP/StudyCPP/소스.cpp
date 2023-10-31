83c1c96682d0f6e20a7bb75b85b24cd5ae88f31f
semin1012 <semin1012@gmail.com>
2023-10-31 15:15:16 +0900

    프로그래머스 #1 대소문자 변환

--- StudyCPP/StudyCPP/소스.cpp
+++ StudyCPP/StudyCPP/소스.cpp
@@ -1,17 +1,39 @@
 //-----------------------------------------------------------------------------
-// 2023년 C++ 공부, 시작하자! C++17 
+// 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로
+// 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
 // 
-// 제 2 장, 변수와 데이터 타입
+// 
+// * 제한사항
+//	 1 ≤ str의 길이 ≤ 20
+//		str은 알파벳으로 이루어진 문자열입니다.
 //-----------------------------------------------------------------------------
 #include <iostream>
+#include <string>
 #include "save.h"
 
-int main() 
-{
-	const char* testString = "이게맞나";
-	char testString2[] = "되나";
+using namespace std;
 
-	std::cout << testString << "   " << testString2 << std::endl;
+int main(void) {
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.length(); i++) {
+        if (isupper(str[i])) str[i] = tolower(str[i]);
+        else str[i] = toupper(str[i]);
+    }
 
-	saveCpp();
+    cout << str;
+    saveCpp();
+
+    // 다른 사람의 풀이 
+    //for (auto c : str)
+    //{
+    //    if ('a' <= c && c <= 'z')
+    //        c -= 'a' - 'A';
+    //    else
+    //        c += 'a' - 'A';
+    //    cout << c;
+    //}
+    //return 0;
+
+    // 
 }
