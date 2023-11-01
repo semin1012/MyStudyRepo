4cfee388012d13af31d033ed5c70be5180dc9277
semin1012 <semin1012@gmail.com>
2023-11-01 11:23:04 +0900

    #3 문자열 겹쳐 쓰기
    a 문자열에 s번째 문자부터 b 문자열로 붙여넣기
    문자열의 .replace 함수를 기억해서 잘 활용하자
    answer에 집착하지 않고 그저 a 문자열을 활용하여 바로 b 문자열로 고쳐 써도 된다

--- StudyCPP/StudyCPP/소스.cpp
+++ StudyCPP/StudyCPP/소스.cpp
@@ -1,11 +1,9 @@
 //-----------------------------------------------------------------------------
-// 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로
-// 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+// 문자열 겹쳐 쓰기
 // 
-// 
-// * 제한사항
-//	 1 ≤ str의 길이 ≤ 20
-//		str은 알파벳으로 이루어진 문자열입니다.
+// 문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
+// 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 문자열 
+// overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
 //-----------------------------------------------------------------------------
 #include <iostream>
 #include <string>
@@ -13,18 +11,39 @@
 
 using namespace std;
 
+string solution(string my_string, string overwrite_string, int s);
+
 int main(void) {
-    string str;
-    cin >> str;
-    for (int i = 0; i < str.length(); i++) cout << str[i] << endl;
+    string str = "He11oWor1d";
+    string str1 = "lloWorld";
+
+    string answer = solution(str, str1, 2);
+    cout << answer << endl;
     saveCpp();
+}
 
+string solution(string my_string, string overwrite_string, int s) {
+    // 내 코드 
+    string answer = "";
+    for (int i = 0; i < my_string.length(); i++) {
+        if (i < s || i > overwrite_string.length() + s - 1)
+            answer += my_string[i];
+        else
+            answer += overwrite_string[i - s];
+    }
+    return answer;
 
+    // 다른 사람 코드
+    /*string answer = "";
+    answer = my_string.replace(s, overwrite_string.size(), overwrite_string);
+    return answer;*/
+    // replace 함수를 사용하는 것 기억하기
 
-    // 다른 사람 풀이
-    /*string str;
-    cin >> str;
-    for (char ch : str)
-        cout << (char)ch << endl;
-    return 0;*/
+    // 다른 사람 코드
+    /*for (auto i = 0; i < overwrite_string.size(); ++i)
+    {
+        my_string[i + s] = overwrite_string[i];
+    }
+    return my_string;*/
+    // answer을 사용하지 않고 그냥 my_string 자체를 변경하는 것
 }
