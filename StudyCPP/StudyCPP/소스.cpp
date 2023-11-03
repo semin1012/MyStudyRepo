4617dc00a2d7caa9d58de5e750d2ddbff646cd2c
semin1012 <semin1012@gmail.com>
2023-11-03 15:43:57 +0900

    #7 홀짝에 따라 다른 값 반환하기 !!

--- StudyCPP/StudyCPP/소스.cpp
+++ StudyCPP/StudyCPP/소스.cpp
@@ -1,7 +1,9 @@
 ﻿//-----------------------------------------------------------------------------
-// 공배수 
+// 홀짝에 따라 다른 값 반환하기
 // 
-// 점수 num 이 n 과 m 의 공배수라면 1 을 리턴한다
+// 양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 
+// 정수의 합을 return 하고 n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 
+// 합을 return 하는 solution 함수를 작성해 주세요.
 //-----------------------------------------------------------------------------
 #include <iostream>
 #include <string>
@@ -10,6 +12,31 @@
 
 using namespace std;
 
-int solution(int number, int n, int m) {
-    return number % n == 0 && number % m == 0 ? 1 : 0;
+int solution(int n) {
+    int answer = 0;
+    if (n % 2 == 0) {
+        while (n) {
+            if (n % 2 == 0)
+                answer += n * n;
+            n--;
+        }
+    }
+    else
+        while (n) {
+            if (n % 2 != 0)
+                answer += n;
+            n--;
+        }
+    return answer;
 }
+
+// 다른 사람 풀이
+//int solution(int n) {
+//    int answer = 0;
+//    while (n > 0)
+//    {
+//        answer += n % 2 == 0 ? n * n : n;
+//        n -= 2;
+//    }
+//    return answer;
+//}
