bd8cf06458eb92b4607e7e6523c70dcb0ec0be8c
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-08 17:01:39 +0900

    백준 10986번 : 나머지 합 구하기

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -2,11 +2,8 @@
 #include <vector>
 using namespace std;
 
-// 백준 11660번 : 구간 합 구하기 2
+// 백준 10986번 : 나머지 합 구하기
 
-
-int arr[1025][1025];
-int dp[1025][1025];
 
 int main()
 {
@@ -14,48 +11,38 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int cnt, num;
+	cin >> cnt >> num;
 
-	int n, q;
-	cin >> n >> q;
-
-	// vector<vector<int>> arr(n + 1, vector<int>(n + 1, 0));
-
-	for (int i = 1; i <= n; i++)
+	vector<long> dp(cnt + 1, 0);	// 합계 계산용
+	vector<long> remain(num + 1, 0);	// 나머지 저장용
+	for (int i = 1; i <= cnt; i++) 
 	{
-		for (int j = 1; j <= n; j++)
-		{
-			cin >> arr[i][j];
-			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i][j];
-
-		}
+		int temp = 0;
+		cin >> temp;
+		dp[i] = dp[i - 1] + temp;
+		cout << dp[i] << " ";
 	}
 
-	cout << "숫자 넣기 끝 " << endl;
+	int result = 0;
 
-	for (int i = 0; i < q; i++)
+	for (int i = 0; i < cnt; i++)
 	{
-		int x1, x2, y1, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-		int result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
-		cout << result << endl;
+		int remainder = dp[i] % num;
+
+		if (remainder == 0)
+			result++;
+
+		remain[remainder]++;
 	}
 
-	/*int x1, x2, y1, y2;
-	for (int i = 0; i < q; i++)
+	for (int i = 0; i < num; i++)
 	{
-		cin >> x1 >> y1 >> x2 >> y2;
+		if (remain[i] > 1)	// 나머지가 2개 이상인 애만 검사한다
+			result = result + (remain[i] * (remain[i] - 1)) / (2 * (2 - 1));
+			// 나머지가 같은 애들 중 2개를 뽑는 경우의 수를 구한다
+			// 컴베이션(순열) 공식을 이용한다, 책에 써둠
+	}
 
-		int sum = 0;
-
-		for (int i = x1; i <= x2; i++)
-		{
-			for (int j = y1; j <= y2; j++)
-			{
-				sum += arr[i][j];
-			}
-		}
-		cout << sum << endl;
-	}*/
-
-
+	cout << endl << result << endl;
 }
