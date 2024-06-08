5f65b869c26fbef126055d10c73563ddec937c28
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-08 16:20:25 +0900

    백준 11660번 : 구간 합 구하기 2
    책에서 나온 코드
    내가 짠 코드
    이상한 부분: vector가 안 됨

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -1,43 +1,61 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-// 백준 11659번 : 구간 합 구하기 4
+// 백준 11660번 : 구간 합 구하기 2
 
-// 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오
 
-// 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 
-// 둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 
-// 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
-
-// 총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+int arr[1025][1025];
+int dp[1025][1025];
 
 int main()
 {
-	int numCnt;
-	int sumCnt;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	cin >> numCnt >> sumCnt;
 
-	int sumNumbers[100001] = {};
+	int n, q;
+	cin >> n >> q;
 
-	for (int i = 1; i <= numCnt; i++)
+	// vector<vector<int>> arr(n + 1, vector<int>(n + 1, 0));
+
+	for (int i = 1; i <= n; i++)
 	{
-		int temp;
-		cin >> temp;
-		sumNumbers[i] = sumNumbers[i-1] + temp;
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> arr[i][j];
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i][j];
 
-		cout << sumNumbers[i] << " ";
+		}
 	}
 
-	for (int i = 0 ; i < sumCnt; i++ )
-	{
-		int left;
-		int right;
-		
-		cin >> left >> right;
+	cout << "숫자 넣기 끝 " << endl;
 
-		cout << sumNumbers[right] - sumNumbers[left - 1] << endl;
+	for (int i = 0; i < q; i++)
+	{
+		int x1, x2, y1, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		int result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+		cout << result << endl;
 	}
+
+	/*int x1, x2, y1, y2;
+	for (int i = 0; i < q; i++)
+	{
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		int sum = 0;
+
+		for (int i = x1; i <= x2; i++)
+		{
+			for (int j = y1; j <= y2; j++)
+			{
+				sum += arr[i][j];
+			}
+		}
+		cout << sum << endl;
+	}*/
+
+
 }
