f92f75492abdc0daf9581686cc04ac46eb016121
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-10 20:17:00 +0900

    백준 2018번 : 연속된 자연수의 합 구하기
    투 포인터 알고리즘을 사용한 것이 중요함.
    result 값을 1로 초기화하는 것은 n 자기 자신만 더했을 때의 경우를 포함했기 때문이다.

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -3,61 +3,52 @@
 #include <algorithm>
 using namespace std;
 
-// 백준 1253번 : 좋은 수
+// 백준 2018번 : 연속된 자연수의 합 구하기
+
+/* 어떠한 자연수 N은, 몇 개의 연속된 자연수의 합으로 나타낼 수 있다.
+당신은 어떤 자연수 N(1 ≤ N ≤ 10, 000, 000)에 대해서, 이 N을 몇 개의 
+연속된 자연수의 합으로 나타내는 가지수를 알고 싶어한다.
+이때, 사용하는 자연수는 N이하여야 한다.
+
+예를 들어, 15를 나타내는 방법은 15, 7 + 8, 4 + 5 + 6, 1 + 2 + 3 + 4 + 5의 4가지가 있다.
+반면에 10을 나타내는 방법은 10, 1 + 2 + 3 + 4의 2가지가 있다.
+
+N을 입력받아 가지수를 출력하는 프로그램을 작성하시오. */
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
 	cout.tie(NULL);
+	cin.tie(NULL);
 
-	int N = 0;
+	int n = 0;
+	cin >> n;
 
-	cin >> N;
+	int result = 1;
 
-	vector<int> nums(N, 0);
+	int start = 1;
+	int end = 1;
+	int sum = 1;
 
-	for (int i = 0; i < N; i++)
-	{
-		cin >> nums[i];
-	}
+	while (end != n) {
+		if (sum == n) {
+			result++;
+			end++;
+			sum += end;
+		}
 
-	sort(nums.begin(), nums.end());
-
-	int result = 0;
-
-	for (int i = 0; i < N; i++)
-	{
-		long find = nums[i];	// 좋은 수인지 판별할 값
-		int start = 0;
-		int end = N - 1;
-
-		while (start < end)
+		else if (sum > n)
 		{
-			int sum = nums[start] + nums[end];
+			sum -= start;
+			start++;
+		}
 
-			if (sum == find)
-			{
-				if (start != i && end != i)
-				{
-					result++;
-					break;
-				}
-
-				else if (start == i)
-					start++;
-
-				else if (end == i)
-					end--;
-			}
-
-			else if (sum > find)
-				end--;
-
-			else if (sum < find)
-				start++;    
+		else {
+			end++;
+			sum += end;
 		}
 	}
 
-	cout << "result: " << result << endl;
+	cout << result << endl;
+
 }
