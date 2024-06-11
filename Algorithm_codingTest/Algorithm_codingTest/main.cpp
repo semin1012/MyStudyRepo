41597456ce4e0124f676be458dd91f251b281435
semin1012 <semin1012@gmail.com>
2024-06-11 21:38:54 +0900

    백준 9498번 : 시험 성적

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -1,14 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 백준 10869번 : 사칙연산
+// 백준 9498 : 시험 성적
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
-	int a, b;
-	cin >> a >> b;
-	cout << a + b << '\n' << a-b << '\n' << a*b << '\n' << a/b << '\n' << a%b;
+	int score;
+	cin >> score;
+	if ( score >= 90)
+		cout << "A";
+	else if (score >= 80)
+		cout << "B";
+	else if (score >= 70)
+		cout << "C";
+	else if ( score >= 60)
+		cout << "D";
+	else 
+		cout << "F";
 }
