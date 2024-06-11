60b118d603782c77d05bf7cf515edeacf58164d0
semin1012 <semin1012@gmail.com>
2024-06-11 22:51:32 +0900

    백준 2490 : 윷놀이

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -1,23 +1,36 @@
 ﻿#include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-// 백준 2480번 : 주사위 세 개
+// 백준 2490 : 윷놀이
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int a, b, c = 0;
-	cin >> a >> b >> c;
-	if ( a == b && a == c )
-		cout << 10000 + (a * 1000);
-	else if ( a == b || a == c )
-		cout << 1000 + a * 100;
-	else if ( b == c)
-		cout << 1000 + b * 100;
-	else {
-		cout << max(max(a, b), c) * 100;
+	vector<vector<int>> n(3, vector<int>(4, -1));
+	int bae[3] = {}, dung[3] = {};
+
+
+	for ( int j = 0 ; j < 3; j++ ) {
+		for ( int i = 0 ; i < 4 ; i++ ) {
+			cin >> n[j][i];
+			if ( n[j][i] == 0 )
+				bae[j]++;
+			else if ( n[j][i] == 1 )
+				dung[j]++;
+		}
+	}
+	for ( int i = 0 ; i < 3; i++ ) { 
+		if ( bae[i] == 1 && dung[i] == 3 )
+			cout << "A\n";
+		else if ( bae[i] == 2 && dung[i] == 2)
+			cout << "B\n";
+		else if ( bae[i] == 3 && dung[i] == 1)
+			cout << "C\n";
+		else if ( bae[i] == 4)
+			cout << "D\n";
+		else cout << "E\n";
 	}
 }
