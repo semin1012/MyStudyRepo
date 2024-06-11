79a429326049f5c1c6c5f591ffd5a14066a6bffc
semin1012 <semin1012@gmail.com>
2024-06-11 22:53:12 +0900

    백준 2490 : 윷놀이 좋은 코드

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -3,34 +3,19 @@
 using namespace std;
 
 // 백준 2490 : 윷놀이
+int result, input;
+string res = "DCBAE";
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	vector<vector<int>> n(3, vector<int>(4, -1));
-	int bae[3] = {}, dung[3] = {};
-
-
-	for ( int j = 0 ; j < 3; j++ ) {
-		for ( int i = 0 ; i < 4 ; i++ ) {
-			cin >> n[j][i];
-			if ( n[j][i] == 0 )
-				bae[j]++;
-			else if ( n[j][i] == 1 )
-				dung[j]++;
-		}
-	}
-	for ( int i = 0 ; i < 3; i++ ) { 
-		if ( bae[i] == 1 && dung[i] == 3 )
-			cout << "A\n";
-		else if ( bae[i] == 2 && dung[i] == 2)
-			cout << "B\n";
-		else if ( bae[i] == 3 && dung[i] == 1)
-			cout << "C\n";
-		else if ( bae[i] == 4)
-			cout << "D\n";
-		else cout << "E\n";
-	}
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+    
+  for(int r = 0; r < 3; r++) {
+    result = 0;
+    for(int c = 0; c < 4; c++) {
+      cin >> input;
+      result += input;
+    }
+    cout << res[result] << '\n';
+  }
 }
