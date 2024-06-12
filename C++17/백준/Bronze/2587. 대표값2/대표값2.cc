abc6cae9b97410f8c81c7219bbebdd341031d73d
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-12 22:37:41 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 백준 2576번 : 홀수
+
+int main(void)
+{
+    vector<int> n(5, 0);
+    int sum = 0;
+    for ( int i = 0 ; i < 5; i++ ) {
+        cin >> n[i];
+        sum += n[i];
+    }
+    sort(n.begin(), n.end());
+    int avg = sum / 5;
+    cout << avg << endl << n[2];
+}
