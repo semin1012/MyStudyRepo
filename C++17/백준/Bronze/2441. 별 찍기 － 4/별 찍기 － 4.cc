8e701804beb78f8b22c495f3fa7c508b2ef3a24e
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-13 22:10:17 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    for ( int i = 0; i < n; i++ ) {
+        for ( int j = 0; j < i; j++ ) cout << " ";
+        for ( int j = 0; j < n-i; j++ ) cout << "*";
+        cout << "\n";
+    }
+}
