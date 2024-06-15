eb84437f8034cf28127580bf6234c4ca99306412
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-15 21:18:56 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int A, B, C;
+  cin >> A >> B >> C;
+  int t=A*B*C;
+  int d[10] = {};
+  // 계산 결과를 자릿수별로 확인하여 저장
+  while (t>0) {
+    d[t%10]++;
+    t/=10;
+  }
+  for (int i=0; i<10; ++i) cout << d[i] << '\n';
+}
