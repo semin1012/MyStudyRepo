6ec1331f43da45a06fc7a9ba11742ef514bca35c
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-07-29 15:00:18 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,71 +1,59 @@
 #include <iostream>
 #include <stack>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	stack<char> s;
-	int answer = 0;
-	int temp = 1;
-	bool result = true;
-	char prev = ' ';
 
 	string str;
+	stack<char> st;
+	char pre;
+	int temp = 1;
+	int aswer = 0;
+	bool isValid = true;
 	cin >> str;
-
-	for (auto c : str)
+	for (char c : str)
 	{
 		if (c == '(' || c == '[')
 		{
-			s.push(c);
-			if (c == '(')
-				temp *= 2;
+			st.push(c);
+			if (c == '(') temp *= 2;
 			else temp *= 3;
-		}
-		else if (c == ')')
-		{
-			if (s.empty() || s.top() != '(')
-			{
-				result = false;
-				break;
-			}
-			s.pop();
-			if (prev == '(')
-			{
-				answer += temp;
-				temp /= 2;
-			}
-			else 
-			{
-				temp /= 2;
-			}
 		}
 		else if (c == ']')
 		{
-			if (s.empty() || s.top() != '[') 
-			{
-				result = false;
+			if (st.empty() || st.top() != '[') {
+				isValid = false;
 				break;
 			}
-			s.pop();
-			if (prev == '[')
+			st.pop();
+			if (pre == '[')
 			{
-				answer += temp;
+				aswer += temp;
 				temp /= 3;
 			}
-			else
-			{
-				temp /= 3;
-			}
+			else temp /= 3;
 		}
-		prev = c;
+		else if (c == ')')
+		{
+			if (st.empty() || st.top() != '(') {
+				isValid = false;
+				break;
+			}
+			st.pop();
+			if (pre == '(')
+			{
+				aswer += temp;
+				temp /= 2;
+			}
+			else temp /= 2;
+		}
+		pre = c;
 	}
-	if (!s.empty()) result = false;
-	if (result != false)
-		cout << answer;
-	else cout << "0";
+
+	if (isValid == false || st.size() > 0) cout << "0\n";
+	else cout << aswer << "\n";
 }
