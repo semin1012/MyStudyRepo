e96e222a1906eb66ec25a42b0aee7cd5c6771c56
semin1012 <semin1012@gmail.com>
2024-12-10 17:13:43 +0900

    삽입 정렬

--- Algorithm_codingTest/Algorithm_codingTest/main.cpp
+++ Algorithm_codingTest/Algorithm_codingTest/main.cpp
@@ -1,33 +1,43 @@
-﻿#include <string>
-#include <vector>
+﻿#include <iostream>
 #include <algorithm>
-#include <map>
-#include <iostream>
-
+#include <vector>
 using namespace std;
 
-vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
-    vector<int> answer;
-    map<string, int> temp;
-    
-    for (int i = 0; i < name.size(); i++)
-        temp.insert(make_pair(name[i], yearning[i]));
+int main()
+{
+	srand(time(0));
+	vector<int> nums = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	random_shuffle(nums.begin(), nums.end());
 
-    for (int i = 0; i < photo.size(); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < photo[i].size(); j++)
-        {
-            for (int z = 0; z < name.size(); z++)
-            {
-                if (name[z] == photo[i][j])
-                {
-                    sum += yearning[z];
-                    break;
-                }
-            }
-        }
-        answer.push_back(sum);
-    }
-    return answer;
+	cout << "정렬 전: ";
+	for (int num : nums)
+		cout << num << " ";
+	cout << endl;
+
+	for (int i = 1; i < nums.size(); i++)
+	{
+		int j = i - 1;
+		while (j >= 0)
+		{
+			if (nums[j] < nums[i])
+			{
+				int temp = nums[j];
+				nums[j] = nums[i];
+				nums[i] = temp;
+				break;
+			}
+			j--;
+		}
+		if (j < 0) j = 0;
+		for (int z = i; z > j; z--)
+		{
+			int temp = nums[z];
+			nums[z] = nums[z - 1];
+			nums[z - 1] = temp;
+		}
+	}
+	cout << "정렬 후: ";
+	for (int num : nums)
+		cout << num << " ";
+	cout << endl;
 }
