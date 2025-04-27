8eedcd998b37fd9174dec9c0a3cbecf511db04db
semin1012 <73764069+semin1012@users.noreply.github.com>
2025-04-28 01:40:22 +0900

    배열 문제 O(N)으로 푸는 방법

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,49 +1,92 @@
 #include <iostream>
 using namespace std;
 
-void insert(int idx, int num, int arr[], int& len){
-    len++;
-    for (int i = len; i > idx; i--)
-        arr[i] = arr[i-1];
-    arr[idx] = num;
+int func1(int N){
+  return -1;
 }
 
-void erase(int idx, int arr[], int& len){
-    len--;
-    for (int i = idx; i < len; i++)
-        arr[i] = arr[i+1];
+// 주어진 길이 N의 int 배열 arr에서 합이 100인 서로 다른 위치의 두 원소가 존재하면
+// 1을, 존재하지 않으면 0을 반환하는 함수 func2(int arr[], int N)을 작성하라.
+// arr의 각 수는 0 이상 100 이하이고 N은 1000 이하이다.
+int func2(int arr[], int N){
+    int temp[1000] = {};
+    for (int i = 0; i < N; i++)
+    {
+        if (temp[100 - arr[i]] == 1) 
+            return 1;
+        temp[arr[i]]++;
+    }
+    return 0;
 }
 
-void printArr(int arr[], int& len){
-  for(int i = 0; i < len; i++) cout << arr[i] << ' ';
-  cout << "\n\n";
+int func3(int N){
+  return -1;
 }
 
-void insert_test(){
-  cout << "***** insert_test *****\n";
-  int arr[10] = {10, 20, 30};
-  int len = 3;
-  insert(3, 40, arr, len); // 10 20 30 40
-  printArr(arr, len);
-  insert(1, 50, arr, len); // 10 50 20 30 40
-  printArr(arr, len);
-  insert(0, 15, arr, len); // 15 10 50 20 30 40
-  printArr(arr, len);
+int func4(int N){
+  return -1;
 }
 
-void erase_test(){
-  cout << "***** erase_test *****\n";
-  int arr[10] = {10, 50, 40, 30, 70, 20};
-  int len = 6;
-  erase(4, arr, len); // 10 50 40 30 20
-  printArr(arr, len);
-  erase(1, arr, len); // 10 40 30 20
-  printArr(arr, len);
-  erase(3, arr, len); // 10 40 30
-  printArr(arr, len);
+void test1(){
+  cout << "****** func1 test ******\n";
+  int n[3] = {16, 34567, 27639};
+  int ans[3] = {60, 278812814, 178254968};
+  for(int i = 0; i < 3; i++){
+    int result = func1(n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+  cout << "*************************\n\n";
 }
 
-int main(void) {
-  insert_test();
-  erase_test();
+void test2(){
+  cout << "****** func2 test ******\n";
+  int arr[3][4] = {{1,52,48}, {50,42}, {4,13,63,87}};
+  int n[3] = {3, 2, 4};
+  int ans[3] = {1, 0, 1};
+  for(int i = 0; i < 3; i++){
+    int result = func2(arr[i], n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+  cout << "*************************\n\n";
+}
+
+void test3(){
+  cout << "****** func3 test ******\n";
+  int n[3] = {9, 693953651, 756580036};
+  int ans[3] = {1, 0, 1};
+  for(int i = 0; i < 3; i++){
+    int result = func3(n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+  cout << "*************************\n\n";
+}
+
+void test4(){
+  cout << "****** func4 test ******\n";
+  int n[3] = {5, 97615282, 1024};
+  int ans[3] = {4, 67108864, 1024};
+  for(int i = 0; i < 3; i++){
+    int result = func4(n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+  cout << "*************************\n\n";
+}
+
+int main(void){
+  test1();
+  test2();
+  test3();
+  test4();
 }
