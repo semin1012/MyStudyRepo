01d1418c571a0685dcb6ad4e52bfd3b65356834f
semin1012 <73764069+semin1012@users.noreply.github.com>
2025-04-28 01:28:03 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- C++17/백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ C++17/백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int main(void) {
-    ios::sync_with_stdio(false);
+int main()
+{
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    string word;
+    cout.tie(NULL);
+
     int alpha[26] = {};
+    string word;
     cin >> word;
-    for ( int i = 0; i < word.size(); i++ ) {
-        for ( int j = 0; j < 26; j++ ) {
-            if ( word[i] - (97+j) == 0 )
-                alpha[j]++;
-        }
-    }
-    for ( int i = 0 ; i < 26; i++ )
+    for (int i = 0; i < word.size(); i++)
+        alpha[word[i] - 'a']++;
+    for (int i = 0; i < 26; i++)
         cout << alpha[i] << " ";
 }
