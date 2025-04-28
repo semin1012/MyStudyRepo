a23606ad64499ba42923ef2b643af2f1b147567a
semin1012 <73764069+semin1012@users.noreply.github.com>
2025-04-29 00:37:50 +0900

    내가 만든 스택

--- C++/test.cpp
+++ C++/test.cpp
@@ -2,61 +2,33 @@
 using namespace std;
 
 const int MX = 1000005;
-int dat[MX], pre[MX], nxt[MX];
-int unused = 1;
+int dat[MX];
+int pos = 0;
 
-void insert(int addr, int num){
-    dat[unused] = num;
-    pre[unused] = addr;
-    nxt[unused] = nxt[addr];
-    if (nxt[addr] != -1) pre[nxt[addr]] = unused;
-    nxt[addr] = unused;
-    unused++;
+void push(int x){
+    dat[pos++] = x;
 }
 
-void erase(int addr){
-    if (nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
-    nxt[pre[addr]] = nxt[addr];
+void pop(){
+    if (pos > 0)
+        pos--;
 }
 
-void traverse(){
-  int cur = nxt[0];
-  while(cur != -1){
-    cout << dat[cur] << ' ';
-    cur = nxt[cur];
-  }
-  cout << "\n\n";
+int top(){
+    return dat[pos-1];
 }
 
-void insert_test(){
-  cout << "****** insert_test *****\n";
-  insert(0, 10); // 10(address=1)
-  traverse();
-  insert(0, 30); // 30(address=2) 10
-  traverse();
-  insert(2, 40); // 30 40(address=3) 10
-  traverse();
-  insert(1, 20); // 30 40 10 20(address=4)
-  traverse();
-  insert(4, 70); // 30 40 10 20 70(address=5)
-  traverse();
-}
-
-void erase_test(){
-  cout << "****** erase_test *****\n";
-  erase(1); // 30 40 20 70
-  traverse();
-  erase(2); // 40 20 70
-  traverse();
-  erase(4); // 40 70
-  traverse();
-  erase(5); // 40
-  traverse();
+void test(){
+  push(5); push(4); push(3);
+  cout << top() << '\n'; // 3
+  pop(); pop();
+  cout << top() << '\n'; // 5
+  push(10); push(12);
+  cout << top() << '\n'; // 12
+  pop();
+  cout << top() << '\n'; // 10
 }
 
 int main(void) {
-  fill(pre, pre+MX, -1);
-  fill(nxt, nxt+MX, -1);
-  insert_test();
-  erase_test();
+	test();
 }
