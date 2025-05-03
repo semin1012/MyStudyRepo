78c34c35ff3720134d13f6996f741c5577f7cf3e
semin1012 <73764069+semin1012@users.noreply.github.com>
2025-05-03 18:21:11 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- C++17/백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ C++17/백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,41 +1,40 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-string board[102];
-int dist[102][102];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
+int visited[101][101];
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int n, m;
-    queue<pair<int, int>> q;
     cin >> n >> m;
+    string board[101];
     for (int i = 0; i < n; i++)
         cin >> board[i];
     for (int i = 0; i < n; i++)
-        fill(dist[i], dist[i]+m, -1);
-    
-    dist[0][0] = 0;
+        fill(visited[i], visited[i]+m, -1);
+    queue<pair<int, int>> q;
     q.push({0, 0});
-    while (q.size() > 0)
+    visited[0][0] = 1;
+
+    while (!q.empty())
     {
-        pair<int, int> pos = q.front(); q.pop();
+        pair<int, int> cur = q.front();
+        q.pop();
+        if (cur.first == m-1 && cur.second == n-1)
+            break;
         for (int i = 0; i < 4; i++)
         {
-            int x = pos.first + dx[i];
-            int y = pos.second + dy[i];
-            if (x < 0 || y < 0 || x >= m || y >= n) continue;
-            if (board[y][x] == '0' || dist[y][x] != -1) continue;
-            dist[y][x] = dist[pos.second][pos.first] + 1;
+            int x = cur.first + dx[i];
+            int y = cur.second + dy[i];
+            if (x < 0 || y < 0 || x >= m|| y >= n) continue;
+            if (visited[y][x] != -1 || board[y][x] == '0') continue;
+            visited[y][x] = visited[cur.second][cur.first] + 1;
             q.push({x, y});
         }
     }
-
-    cout << dist[n-1][m-1] + 1;
+    cout << visited[n-1][m-1];
 }
